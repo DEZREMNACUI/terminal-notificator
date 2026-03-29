68dc9fb4f6de683c93f2aea6eefe7af581e192c5
zed.luo <dezremnacui@gmail.com>
2026-03-29 21:23:39 +0800

    chore: 提升最低平台版本到 macOS 15

--- terminal-notificator-swift/Package.swift
+++ terminal-notificator-swift/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "TerminalNotificator",
     platforms: [
-        .macOS(.v11)
+        .macOS("15.0")
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
